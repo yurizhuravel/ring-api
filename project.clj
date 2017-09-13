1199364767b50375cfe27a3a79451b06bce5102a
Yuri Zhuravel <zhuravel@gmail.com>
2017-09-13 22:58:06 +0100

    refactoring

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
 (defproject ring-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A basic Clojure Postgres service with Ring"
+  :url ""
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                [org.clojure/java.jdbc "0.7.1"]
-                [ring/ring-core "1.6.2"]
-                [ring/ring-jetty-adapter "1.5.0"]
+                 [org.clojure/java.jdbc "0.7.1"]
+                 [ring/ring-core "1.6.2"]
+                 [ring/ring-jetty-adapter "1.5.0"]
                  [org.postgresql/postgresql "9.2-1003-jdbc4"]]
   :main ^:skip-aot ring-api.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies  [[clj-http "3.7.0"]]}})
+             :dev     {:dependencies [[clj-http "3.7.0"]]}})
--- src/ring_api/core.clj
+++ src/ring_api/core.clj
@@ -1,21 +1,21 @@
 (ns ring-api.core
   (:gen-class)
   (:require [ring.adapter.jetty :as jetty]
-    [clojure.java.jdbc :as jdbc]
-    ))
+            [clojure.java.jdbc :as jdbc]
+            ))
 
 (def http-port 3000)
 (def https-port 3001)
 
-(def db-spec {:password "password"
-              :classname "org.postgresql.Driver",
+(def db-spec {:password    "password"
+              :classname   "org.postgresql.Driver",
               :subprotocol "postgresql",
-              :subname "//localhost:5432/ring-api-test"})
+              :subname     "//localhost:5432/ring-api-test"})
 
 (defn make-response [status body]
-  { :headers {"Content-Type" "text/html"}
-    :status status
-    :body body })
+  {:headers {"Content-Type" "text/html"}
+   :status  status
+   :body    body})
 
 (defn root-handler [request]
   (make-response 200 "Welcome to Root"))
@@ -24,26 +24,26 @@
   (make-response 200 "Welcome to the test data route"))
 
 (defn create-table-if-not-exists [table-name]
-      (try
-        (seq? (jdbc/query db-spec [(str "select * from " table-name)]))
-        (catch Exception e
-          (println (str "Table doesn't exist " table-name ", creating"))
-          (jdbc/query db-spec [(str "create table " table-name " (id serial, count int)")]))))
+  (try
+    (seq? (jdbc/query db-spec [(str "select * from " table-name)]))
+      (catch Exception e
+        (println (str "Table doesn't exist " table-name ", creating"))
+        (jdbc/query db-spec [(str "create table " table-name " (id serial, count int)")]))))
 
 (defn increment-counter []
-      (let [old-count (:count (jdbc/get-by-id db-spec :counter 1))]
-      (jdbc/update! db-spec :counter {:count (inc old-count)} ["id = ?" 1])
-      old-count))
+  (let [old-count (:count (jdbc/get-by-id db-spec :counter 1))]
+    (jdbc/update! db-spec :counter {:count (inc old-count)} ["id = ?" 1])
+    old-count))
 
 (defn counter-handler [request]
-      (let [counter (increment-counter)]
-      (make-response 200 (str counter))))
+  (let [counter (increment-counter)]
+    (make-response 200 (str counter))))
 
 (defn handler [request]
   (case (:uri request)
     "/" (root-handler request)
     "/test-data" (test-data-handler request)
-        "/counter" (counter-handler request)
+    "/counter" (counter-handler request)
     (make-response 404 "Route Not Found")))
 
 (defonce running-server (atom nil))
@@ -51,14 +51,14 @@
 (defn stop-server [] (reset! running-server (.stop @running-server)))
 
 (defn start-server [] (if @running-server
-  nil
-  (reset! running-server (jetty/run-jetty handler
-    { :port http-port
-      ; :ssl? true
-      ; :ssl-port https-port
-      ; :keystore "keystore.jks"
-      ; :key-password "simpsons"
-      :join? false}))))
+                        nil
+                        (reset! running-server (jetty/run-jetty handler
+                                                                {:port  http-port
+                                                                 ; :ssl? true
+                                                                 ; :ssl-port https-port
+                                                                 ; :keystore "keystore.jks"
+                                                                 ; :key-password "simpsons"
+                                                                 :join? false}))))
 
 (defn -main
   "Start API server."
